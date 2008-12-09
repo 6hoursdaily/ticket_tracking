2e84b13945d04be5d52118069e6ae892404255ba
Alejandro Juarez <alexjr85@gmail.com>
2008-12-09 13:16:56 -0600

    Adding support for twitter account

--- db/migrate/20081202233153_create_users.rb
+++ db/migrate/20081202233153_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
       t.string :login, :null => false
-      t.string :password, :email
+      t.string :password, :email, :twitter
       t.boolean :status, :is_admin, :null => false, :default => false
       t.timestamps
     end
